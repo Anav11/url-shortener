3da7786b72fb421fd70aa3927bfc6dce842017f0
andrey <a.averin@qiwi.com>
2022-02-19 16:42:25 +0500

    read body

--- cmd/shortener/server/handlers.go
+++ cmd/shortener/server/handlers.go
@@ -2,8 +2,8 @@ package server
 
 import (
 	"fmt"
-	"github.com/Anav11/url-shortener/internal/templates"
 	"github.com/Anav11/url-shortener/internal/util"
+	"io"
 	"net/http"
 )
 
@@ -11,21 +11,25 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		if r.URL.Path == "/" {
-			fmt.Fprint(w, templates.Form)
+			http.Error(w, "Bad Request", http.StatusBadRequest)
 		} else {
 			initialURL := util.GetURL(r.URL.Path)
 			w.Header().Set("Location", initialURL)
 			w.WriteHeader(http.StatusTemporaryRedirect)
 		}
 	case http.MethodPost:
-		url := r.FormValue("url")
-		shortPath := util.URLShortener(url)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusInternalServerError)
+			return
+		}
+		shortPath := util.URLShortener(string(body))
 		shortURL := fmt.Sprintf("http://%s/%s", r.Host, shortPath)
 
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(201)
 		w.Write([]byte(shortURL))
 	default:
-		http.Error(w, "Bad Request", 400)
+		http.Error(w, "Bad Request", http.StatusBadRequest)
 	}
 }
