68dc9c3df98903426b77d4edca1d65e228d878bb
andrey <a.averin@qiwi.com>
2022-04-17 13:28:17 +0500

    Increment 5.
    env variable

--- cmd/shortener/server/server.go
+++ cmd/shortener/server/server.go
@@ -1,20 +1,23 @@
 package server
 
 import (
-	"fmt"
+	"github.com/caarlos0/env/v6"
 
 	"github.com/Anav11/url-shortener/internal/app"
 	"github.com/Anav11/url-shortener/internal/app/router"
 	"github.com/Anav11/url-shortener/internal/app/storage"
 )
 
-func Start(port int) {
-	c := app.Config{
-		Host: "http://localhost",
-		Port: port,
+func Start() {
+	c := app.Config{}
+	if err := env.Parse(&c); err != nil {
+		return
 	}
+
 	s := storage.ConstructStorage()
 	r := router.Router(c, s)
 
-	r.Run(fmt.Sprintf(":%d", c.Port))
+	if err := r.Run(c.ServerAddress); err != nil {
+		panic(err)
+	}
 }
